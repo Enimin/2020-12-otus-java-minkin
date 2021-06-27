00e3a8b363479e98e7dfdcc64eff77eb7a60642b
Evgeny Minkin <men@mail.ru>
2021-06-27 10:03:56 +0300

    hw32-messagingSystem
    Удалены лишние файлы

--- hw32-messagingSystem/src/main/java/ru/rt/requestDataTypes/ClientResponseDT.java
+++
@@ -1,6 +0,0 @@
-package ru.rt.requestDataTypes;
-
-import ru.otus.messagesystem.client.ResultDataType;
-
-public class ClientResponseDT  extends ResultDataType {
-}
--- hw32-messagingSystem/src/main/java/ru/rt/resultsDT/ClientResultDataType.java
+++
@@ -1,7 +0,0 @@
-package ru.rt.resultsDT;
-
-import ru.otus.messagesystem.client.ResultDataType;
-
-public interface ClientResultDataType<T> {
-    T getResult();
-}
